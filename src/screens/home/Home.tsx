3857682be508199a9313b117000dcdfb6ede54e3
Egor Mikheev <egor.mikheev@humaniq.com>
2022-08-04 13:28:21 +0300

    initial commit for template

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,15 +1,8 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { observer } from "mobx-react";
 import { HomeViewModel } from "./HomeViewModel";
-import { UserStore, UserStore as user } from "../../stores/userStore";
 import { withStore } from "../../utils/hoc";
 import "./Home.sass";
-import { HumaniqIdCard } from "../../components/humaniq-id-card/HumaniqIdCard";
-import { getProviderStore } from "../../App";
-import { Button } from "@mui/material";
-import { NotRegisteredMessage } from "../../components/not-registered-message/NotRegisteredMessage";
-import { HumaniqIdForm } from "../../components/humaniq-id-form/HumaniqIdForm";
-import { t } from "i18next";
 import { ConnectDialog } from "../../components/dialogs/ConnectDialog";
 import { DisconnectDialog } from "../../components/dialogs/DisconnectDialog";
 
@@ -18,67 +11,10 @@ export interface HomeScreenInterface {
 }
 
 const HomeImpl = ({ store: view }: HomeScreenInterface) => {
-  const inputGalleryRef = useRef<any>();
-
-  useEffect(() => {
-    (async () => {
-      await view.init(inputGalleryRef);
-    })();
-  }, [view]);
-
   return (
-    <div className={view.isEditMode ? "container edit" : "container"}>
+    <div className={"container"}>
       <div className={"main"}>
-        {!view.isEditMode && (
-          <HumaniqIdCard
-            verified={!user.isAnyFieldEmpty}
-            address={getProviderStore.currentAccount}
-            fields={[
-              ["Date of birth", user.birthDate],
-              ["Country", user.country],
-              ["City", user.city],
-            ]}
-            name={user.fullName}
-            photoUrl={user.photoURI}
-            loading={!view.initialized}
-          />
-        )}
-        {view.isEditMode && (
-          <HumaniqIdForm
-            user={UserStore}
-            onBackClick={view.backClick}
-            onFileChoose={view.onFileChoose}
-            openFileExplorer={view.openFileExplorer}
-            inputRef={view.galleryRef}
-          />
-        )}
-        {!view.isEditMode && (
-          <NotRegisteredMessage
-            isRegistered={user.isRegistered}
-            isConnected={getProviderStore.currentAccount}
-          />
-        )}
-        {getProviderStore.currentAccount && (
-          <Button
-            onClick={view.toggleEditOrSave}
-            className={view.isEditMode ? "btn" : "btn-2"}
-            variant={"contained"}
-            disabled={view.isEditMode ? user.isAnyFieldEmpty : false}
-          >
-            {view.isEditMode ? t("signWithWallet") : t("egitProfile")}
-          </Button>
-        )}
-        {!view.isEditMode && (
-          <Button
-            onClick={view.toggleDialogOrDisconnectWallet}
-            className={"btn"}
-            variant={!getProviderStore.currentAccount ? "contained" : "text"}
-          >
-            {!getProviderStore.currentAccount
-              ? t("connectWalletDialog")
-              : t("disconnect")}
-          </Button>
-        )}
+        <h1>Humaniq Dapps Template</h1>
         <ConnectDialog />
         <DisconnectDialog />
       </div>
--- src/screens/home/HomeViewModel.ts
+++ src/screens/home/HomeViewModel.ts
@@ -1,69 +1,12 @@
 import { RefObject } from "react";
-import { makeAutoObservable, reaction } from "mobx";
-import { UserStore, UserStore as user } from "../../stores/userStore";
-import { ETHProvider } from "../../stores/providerStore";
-import { getProviderStore } from "../../App";
-import Logcat from "../../utils/logcat";
+import { makeAutoObservable } from "mobx";
 
 export class HomeViewModel {
   galleryRef?: RefObject<HTMLInputElement>;
 
-  isEditMode = false;
   initialized = false;
 
   constructor() {
     makeAutoObservable(this, {}, { autoBind: true });
   }
-
-  backClick = async () => {
-    user.onReset();
-    this.isEditMode = false;
-    await user.fetchProfile();
-  };
-
-  init = async (ref?: RefObject<HTMLInputElement>) => {
-    this.galleryRef = ref;
-    ETHProvider.currentAccount && (await user.fetchProfile());
-    reaction(
-      () => ETHProvider.currentAccount,
-      async (val) => {
-        if (val) {
-          await user.fetchProfile();
-        } else {
-          UserStore.onReset();
-        }
-      }
-    );
-    this.initialized = true;
-  };
-
-  toggleEditOrSave = async () => {
-    if (this.isEditMode) {
-      const result = await user.onSubmit();
-      if (result) {
-        this.isEditMode = !this.isEditMode;
-      }
-    } else {
-      this.isEditMode = !this.isEditMode;
-    }
-  };
-
-  openFileExplorer = () => {
-    this.galleryRef?.current?.click();
-  };
-
-  onFileChoose = async (event: any) => {
-    event.preventDefault();
-    await user.updatePhoto(event.target.files[0]);
-  };
-
-  toggleDialogOrDisconnectWallet = () => {
-    if (!getProviderStore.currentAccount) {
-      getProviderStore.connectDialog = !getProviderStore.connectDialog;
-    } else {
-      this.isEditMode = false;
-      getProviderStore.toggleDisconnectDialog();
-      // getProviderStore.disconnect();
-    }
-  };
 }
